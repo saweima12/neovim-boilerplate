1fe993b37626dc3d76cd134b20705b123724a1b0
Saweima <62002212+saweima12@users.noreply.github.com>
2023-08-23 15:09:31 +0800

    Change language encode

--- lua/settings.lua
+++ lua/settings.lua
@@ -11,7 +11,7 @@ cmd [[filetype plugin on]]
 -- Disable comments on pressing Enter
 cmd [[autocmd FileType * setlocal formatoptions-=cro]]
 
-cmd [[language en_US]]
+cmd [[language en_US.UTF-8]]
 
 -- Set leader key
 g.mapleader = '\\'
